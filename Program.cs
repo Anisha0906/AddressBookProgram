5e42f8232f7793295ceaf710fcc016620c506ad0
Anisha0906 <anishadas880@gmail.com>
2023-01-11 19:01:30 +0530

    [Anisha]Added:Ability to Add a new contact

--- Program.cs
+++ Program.cs
@@ -12,9 +12,31 @@ namespace AddressBook
       {
             Console.WriteLine("Welcome To Address Book Program");
             AddressBook addressBook = new AddressBook();
-            addressBook.addContacts("Anisha", "Das", "Sunabeda", "Koraput", "Odisha", 763001, 01234567890, "abcd@gmail.com");
+            TakeInputAndAddToContacts(addressBook);
+            //takeInputAndAddToContacts(addressBook);
             addressBook.print();
             Console.ReadLine();
       }
+        public static void TakeInputAndAddToContacts(AddressBook addressBook)
+        {
+            Console.WriteLine("Enter FirstName");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter LastName");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter Address");
+            string address = Console.ReadLine();
+            Console.WriteLine("Enter City");
+            string city = Console.ReadLine();
+            Console.WriteLine("Enter State");
+            string state = Console.ReadLine();
+            Console.WriteLine("Enter Zip");
+            int zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter PhoneNumber");
+            long phoneNumber = long.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Email id");
+            string email = Console.ReadLine();
+            addressBook.addContacts(firstName, lastName, address, city, state, zip, phoneNumber, email);
+            Console.ReadLine();
+        }
     }
 }
